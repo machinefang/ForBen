fc6b7a868c593562aef0d93f6556c700040c3ce5
Dongxu <liu.don@husky.neu.edu>
2016-05-01 10:40:57 -0400

    Initial Commit

---
+++ BenLostWeight/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BenLostWeight
+//
+//  Created by LiuDongxu on 5/1/16.
+//  Copyright © 2016 Xin Fang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
